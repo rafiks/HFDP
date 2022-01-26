5790dab8e2c415ae5e2c27d54c670ac9b857bea5
rafiks <rab@devsphoto.com>
2022-01-26 17:25:17 -0500

    added interfaces

---
+++ src/com/weather/Subject.java
@@ -0,0 +1,9 @@
+package com.weather;
+
+import java.util.Observer;
+
+public interface Subject {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+}
