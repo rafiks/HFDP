327398794f481d5f92c440d5330eb3acdf7f5f9d
rafiks <rab@devsphoto.com>
2022-01-26 20:08:19 -0500

    Flyrocketpowered model duck

--- src/com/devsphoto/Duck.java
+++ src/com/devsphoto/Duck.java
@@ -1,19 +1,32 @@
 package com.devsphoto;
+
 public abstract class Duck {
- FlyBehavior flyBehavior;
- QuackBehavior quackBehavior;
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
 
- public Duck(){
+    public Duck() {
 
- }
- public abstract void display();
- public void performFly(){
-     flyBehavior.fly();
- }
-public void performQuack(){
-     quackBehavior.quack();
-}
-public void swim(){
-     System.out.println("All ducks float, even decoys!");
-}
+    }
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+
+    public void setFlyBehavior(FlyBehavior fb) {
+       flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
 }
--- src/com/devsphoto/FlyNoWay.java
+++ src/com/devsphoto/FlyNoWay.java
@@ -1,7 +1,7 @@
 package com.devsphoto;
 
-public class FlyNoWay implements  FlyBehavior{
-    public void fly(){
+public class FlyNoWay implements FlyBehavior {
+    public void fly() {
         System.out.println("I can't fly.");
     }
 }
