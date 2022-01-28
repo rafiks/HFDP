d85c72bb2f595d83ec088dd7d08b6b5b427783d9
rafiks <rab@devsphoto.com>
2022-01-27 23:57:44 -0500

    WeatherStation complete

--- src/com/weather/Subject.java
+++ src/com/weather/Subject.java
@@ -1,9 +1,7 @@
 package com.weather;
 
-import java.util.Observer;
-
 public interface Subject {
-    public void registerObserver(Observer o);
-    public void removeObserver(Observer o);
-    public void notifyObservers();
+    void registerObserver(Observer o);
+    void removeObserver(Observer o);
+    void notifyObservers();
 }
